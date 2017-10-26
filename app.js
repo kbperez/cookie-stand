65d906eb9f50a6e2a524965fc2952240b0cd923e
kbperez <kbpgperez@hotmail.com>
2017-10-25 18:24:51 -0700

    Working as manual data input

--- app.js
+++ app.js
@@ -44,31 +44,16 @@ for (var i = 0; i < times.length; i++)  {
   }
  }
 
-// Instantiation
-var pikeAndFirst = new Store(23, 65, 6.3, '1st and Pike');
-var seaTac = new Store(3, 24, 1.2, 'SeaTac Airport');
-var seatCtr = new Store(11, 38, 3.7, 'Seattle Center');
-var capHill = new Store(20, 38, 2.3, 'Capitol Hill');
-var alki = new Store(2, 16, 4.6, 'Alki');
-
-sites.push(pikeAndFirst);
-sites.push(seaTac);
-sites.push(seatCtr);
-sites.push(capHill);
-sites.push(alki);
-console.log('sites',sites);
-
 
 //Create an array with the store location first, the hourly sales data, and total  location sales, converts array to a string and adds a table row
 
 Store.prototype.tableRow = function () {
-  for (var i = 0; i < sites.length; i++) {
-    var rowInfo = ['<td>' + sites[i].location + '</td>'];
+    var rowInfo = ['<td>' + this.location + '</td>'];
     console.log('begin',rowInfo);
-    for(var j=0; j < sites[i].hrSales.length; j++) {
-      rowInfo.push('<td>' + sites[i].hrSales[j] + '</td>');
+    for(var j=0; j < this.hrSales.length; j++) {
+      rowInfo.push('<td>' + this.hrSales[j] + '</td>');
     }
-    rowInfo.push('<td>' + sites[i].dailyCookies + '</td>');
+    rowInfo.push('<td>' + this.dailyCookies + '</td>');
 
   console.log(rowInfo);
 
@@ -80,9 +65,9 @@ Store.prototype.tableRow = function () {
   newRow = document.createElement('tr');
   newRow.innerHTML = rowInfoStr;
   table.appendChild(newRow);
-  }
-}
 
+}
+//Function to receive input
 function formData(event) {
   event.preventDefault();
 
@@ -92,15 +77,11 @@ function formData(event) {
   var avg_cookies = parseFloat(event.target.avg_cookies.value);
 
   console.log(stand, min_cust, max_cust, avg_cookies);
-
-  data.push(new Store(min_cust, max_cust, avg_cookies, stand));
-  //createTable();
-  //form.reset();
+  var storeOne = new Store(min_cust, max_cust, avg_cookies, stand);
+  storeOne.tableRow();
+  console.log(storeOne);
+  form.reset();
 }
 
+
 form.addEventListener('submit', formData);
-
-
-
-
-pikeAndFirst.tableRow();
